04d3e9516517e2a08055fee46f49c3fd0e042437
Mohammad-h <784709@pdsb.net>
2023-11-18 21:01:18 -0700

    Create function to return book data (not working)

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-// Google Books API Key: 
 
+// Creates an async function which returns the data of a book from the input of a user
 async function searchBooks() {
-  let apiKey = AIzaSyAA2EnEj0qB9xtfoLsN2RrQQDKLq40GHHs
+
+  // Declares a variable for an API key for google books
+  let apiKey = 'AIzaSyAA2EnEj0qB9xtfoLsN2RrQQDKLq40GHHs';
   let title = document.getElementById("search").value;
-  const endpoint = new URL(`https://www.googleapis.com/books/v1/volumes?q=${ title }`)
+  console.log(title)
+
+  let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${ title }s&key=${ apiKey }`);
+  let bookData = await response.json();
+
+  return bookData
 }
+
+searchBooks()
+  .then(data => console.log(data))
+  .catch(reason => console.log(message.reason));
